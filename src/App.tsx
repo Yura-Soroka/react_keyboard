ce28b897dba2726e87599f621eec22c61714c5f6
Yura Soroka <ykotov18@gmail.com>
2025-06-04 13:18:32 +0300

    sol

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+export class App extends React.Component {
+  state= {
+    pressedKey: null,
+  };
+
+  hendleDocumentKeyUp = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: event.key });
+  };
+
+  componentDidMount(): void {
+    document.addEventListener('keyup', this.hendleDocumentKeyUp);
+  }
+
+  componentWillUnmount(): void {
+    document.addEventListener('keyup', this.hendleDocumentKeyUp);
+  }
+
+  render(): React.ReactNode {
+    const { pressedKey } = this.state;
+
+    return (
+      <div className="App">
+        {pressedKey ? (
+          <p className="App__message">The last pressed key is [{pressedKey}]</p>
+        ) : (
+          <p className="App__message">Nothing was pressed yet</p>
+        )}
+      </div>
+    );
+  }
+}
